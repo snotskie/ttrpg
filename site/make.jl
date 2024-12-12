1f7f207fc5eff46030a19ef2c29ca57f84b1a32c
snotskie <snotskie@gmail.com>
2024-12-11 20:32:15 -0600

    toc

--- site/make.jl
+++ site/make.jl
@@ -3,10 +3,12 @@ using Documenter
 makedocs(
   sitename="Humanity, Blessed: TTRPG",
   format=Documenter.HTML(
-        assets=[
-            "assets/style.css",
-            # "assets/script.js"
-        ]
-    )
+      assets=[
+          "assets/style.css",
+          # "assets/script.js"
+      ]
+  ),
+  pages=["index.md", "playing.md", "monstrosities.md"]
 )
+
 deploydocs(repo="github.com/snotskie/ttrpg.git")
