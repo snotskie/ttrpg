117d21be2ab9e3725ea13b3199681c8e0bd8c11c
snotskie <snotskie@gmail.com>
2024-12-21 16:36:25 -0600

    footer

--- site/make.jl
+++ site/make.jl
@@ -6,7 +6,8 @@ makedocs(
       assets=[
           "assets/style.css",
           # "assets/script.js"
-      ]
+      ],
+      footer="[Humanity, Blessed](https://snotskie.github.io/ttrpg/) © 2024 [snotskie](https://snotskie.com/) is licensed under [CC BY-NC-ND 4.0 ![](https://mirrors.creativecommons.org/presskit/icons/nc.svg?ref=chooser-v1)](https://creativecommons.org/licenses/by-nc-nd/4.0/?ref=chooser-v1)"
   ),
   pages=["index.md", "characters.md", "playing.md"]
 )
