8d7e6deb2630413b1d458ab3acae7dc203a61d3c
snotskie <snotskie@gmail.com>
2024-12-31 06:49:19 -0600

    add quick to toc

--- site/make.jl
+++ site/make.jl
@@ -9,7 +9,7 @@ makedocs(
       ],
       footer="[Humanity, Blessed](https://snotskie.github.io/ttrpg/) © 2024 [snotskie](https://snotskie.com/) is licensed under [CC BY-NC-ND 4.0](https://creativecommons.org/licenses/by-nc-nd/4.0/?ref=chooser-v1)"
   ),
-  pages=["index.md", "characters.md", "playing.md"]
+  pages=["index.md", "characters.md", "playing.md", "quick.md"]
 )
 
 deploydocs(repo="github.com/snotskie/ttrpg.git")
