a94cba81678c0c456759eb92bde638a21b77914c
snotskie <snotskie@gmail.com>
2025-04-01 21:48:46 -0500

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,9 +1,3 @@
-function toggleModes(){
-    document.querySelectorAll('section').forEach(
-        (e) => e.classList.toggle('invert')
-    );
-}
-
 function bwbeforeprint(){
     document.querySelectorAll('section').forEach(
         (e) => e.classList.add('bw')
@@ -28,22 +22,6 @@ function bwprint(){
     window.print();
 }
 
-function toggleGuides(){
-    document.querySelectorAll('section.guide').forEach(
-        (e) => e.closest("svg").classList.toggle('secret')
-    );
-
-    localStorage.setItem("view", (localStorage.getItem("view")==="GM")? "Player" : "GM");
-}
-
-function toggleAutoscale(){
-    document.querySelectorAll('body > svg').forEach(
-        (e) => e.classList.toggle('autoscale')
-    );
-
-    localStorage.setItem("zoom", (localStorage.getItem("zoom")==="manual")? "auto" : "manual");
-}
-
 function restyleIndexEntry(name){
     const parts = name.trim().split(" ");
     if (parts.length < 2){
@@ -56,32 +34,10 @@ function restyleIndexEntry(name){
 }
 
 window.addEventListener("load", function(){
-    // match user preferred theme
-    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches){
-        toggleModes();
-    }
-
-    // hide guides
-    if (localStorage.getItem("view") !== "GM"){
-        toggleGuides();
-        localStorage.setItem("view", "Player");
-    } else {
-        document.getElementById("guide-hider").checked = true;
-        localStorage.setItem("view", "GM");
-    }
-
-    // autoscale or not
-    if (('ontouchstart' in window) || (navigator.maxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0)){ // always on mobile
-        toggleAutoscale();
-        localStorage.setItem("zoom", "auto");
-        document.getElementById("autoscaler").closest("label").style.display = "none";
-    } else if (localStorage.getItem("zoom") === "auto"){ // else only if user requested it
-        toggleAutoscale();
-        localStorage.setItem("zoom", "auto");
-        document.getElementById("autoscaler").checked = true;
-    } else { // else just leave default: manual
-        localStorage.setItem("zoom", "manual");
-    }
+    MarpShims.autoScale();
+    MarpShims.breakPages();
+    MarpShims.autoTheme();
+    MarpShims.anchorHeadings("h1,h2,h3,h4,.ability th");
 
     // table of contents
     var i = 0;
@@ -116,19 +72,6 @@ window.addEventListener("load", function(){
             } else if (i === 38){
                 toc = document.getElementById("toc3");
             }
-        }
-    });
-
-    // clickable headings
-    document.querySelectorAll("h1,h2,h3,h4,.ability th").forEach(function(e){
-        const section = e.closest("section");
-        if (parseInt(section.getAttribute("id")) > 1){
-            const html = e.innerHTML;
-            e.innerHTML = "";
-            const a = document.createElement("a");
-            e.appendChild(a);
-            a.innerHTML = html;
-            a.setAttribute("href", "#" + section.getAttribute("id"));
         }
     });
 
@@ -194,12 +137,6 @@ window.addEventListener("load", function(){
             abix = document.getElementById("abix2");
         }
     }
-    
-    // loading...
-    document.body.classList.add("loaded");
 
-    // force hashes to work after loading
-    if (this.location.hash){
-        document.getElementById(location.hash.substring(1)).scrollIntoView();
-    }
+    MarpShims.scrollToHash();
 });
