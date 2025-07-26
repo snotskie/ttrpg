32456db9b8ffc68ea07ef244bb0a8810f8f8b7d8
snotskie <snotskie@gmail.com>
2025-07-25 19:06:53 -0500

    growing pages, accessibly styling tweaks

--- public/script.js
+++ public/script.js
@@ -62,108 +62,11 @@ window.addEventListener("load", function(){
       restyleIndexEntries(ab_records);
       MarpShims.breakPages();
     });
+
+    MarpShims.growPages();
   
     MarpShims.autoTheme();
     MarpShims.anchorHeadings("h1,h2,h3,h4,.ability th");
-
-//     // table of contents
-//     var i = 0;
-//     
-//     document.querySelectorAll("h2,h3").forEach(function(e){
-//         const section = e.closest("section");
-//         const page = section.getAttribute("data-marpit-pagination");
-//         if (page){
-//             const toc_line = document.createElement("p");
-//             toc.appendChild(toc_line);
-//             var entry = document.createElement("em");
-//             if (e.nodeName === "H2"){
-//                 entry = document.createElement("strong");
-//                 toc_line.classList.add("chapter");
-//             }
-
-//             toc_line.appendChild(entry);
-
-//             const entry_a = document.createElement("a");
-//             entry.appendChild(entry_a);
-//             entry_a.innerText = e.innerText;
-//             entry_a.setAttribute("href", "#" + section.getAttribute("id"));
-
-//             const page_no = document.createElement("span");
-//             toc_line.appendChild(page_no);
-//             page_no.classList.add("page_no");
-//             page_no.innerText = page;
-
-//             ++i;
-//             if (i === 18){
-//                 toc = document.getElementById("toc2");
-//             } else if (i === 39){
-//                 toc = document.getElementById("toc3");
-//             }
-//         }
-//     });
-
-//     // table listing
-//     const tablist = document.getElementById("tablist");
-//     document.querySelectorAll("h4").forEach(function(e){
-//         const section = e.closest("section");
-//         const page = section.getAttribute("data-marpit-pagination");
-//         if (page){
-//             const table_line = document.createElement("span");
-//             tablist.appendChild(table_line);
-
-//             const entry = document.createElement("span");
-//             table_line.appendChild(entry);
-
-//             const entry_a = document.createElement("a");
-//             entry.appendChild(entry_a);
-//             entry_a.innerText = e.innerText;
-//             entry_a.setAttribute("href", "#" + section.getAttribute("id"));
-
-//             const page_no = document.createElement("span");
-//             page_no.classList.add("page_no");
-//             table_line.appendChild(page_no);
-//             table_line.appendChild(document.createElement("br"));
-//             page_no.innerText = page;
-//         }
-//     });
-
-//     // ability index
-//     abix_entry_map = {};
-//     abix_entry_keys = [];
-//     document.querySelectorAll(".ability th").forEach(function(e){
-//         const section = e.closest("section");
-//         const page = section.getAttribute("data-marpit-pagination");
-//         if (page){
-//             const abix_line = document.createElement("span");
-//             const entry = document.createElement("span");
-//             abix_line.appendChild(entry);
-
-//             const entry_a = document.createElement("a");
-//             entry.appendChild(entry_a);
-//             entry_a.innerText = restyleIndexEntry(e.innerText.replace(/ (d6|d8|d10|One Free)/, ""));
-//             entry_a.setAttribute("href", "#" + section.getAttribute("id"));
-            
-//             const page_no = document.createElement("span");
-//             page_no.classList.add("page_no");
-//             abix_line.appendChild(page_no);
-//             abix_line.appendChild(document.createElement("br"));
-//             page_no.innerText = page;
-//             abix_entry_map[entry.innerText] = abix_line;
-//             abix_entry_keys.push(entry.innerText);
-//         }
-//     });
-
-//     i = 0;
-//     var abix = document.getElementById("abix");
-//     abix_entry_keys.sort();
-//     console.log(abix_entry_keys);
-//     for (const key of abix_entry_keys){
-//         abix.appendChild(abix_entry_map[key]);
-//         ++i;
-//         if (i === 53){
-//             abix = document.getElementById("abix2");
-//         }
-//     }
 
     MarpShims.scrollToHash();
 });
