0f11539fde9b15385cf600a44e18e01ab0b98e03
snotskie <snotskie@gmail.com>
2025-09-05 10:27:09 -0500

    json export

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
 
 TAG=`git describe --tags --abbrev=0` npx marp --html --template=bare --input-dir ./md -o ./public 
+node export
---
+++ export.js
@@ -0,0 +1,50 @@
+const jsdom = require("jsdom");
+const fs = require("node:fs");
+
+const ex = {
+    "books": {},
+    "options": {}
+};
+
+const sources = [
+    "public/index.html",
+    "public/endeavored.html"
+]
+
+for (const source of sources){
+    const { window } = new jsdom.JSDOM(
+        fs.readFileSync(source, {"encoding": "utf-8"}),
+        {"url": "https://snotskie.com/ttrpg/"}
+    );
+
+    const book_title = window.document.querySelector("h1").textContent.trim();
+    const book_guid = book_title.toLowerCase().replaceAll(/[^a-z0-9]/g, "");
+    ex.books[book_guid] = {
+        "title": book_title,
+        "pages": {}
+    }
+
+    for (const page of window.document.querySelectorAll("svg > foreignObject > section")){
+        const page_title = page.querySelector("h3")?.textContent.trim();
+        if (page_title && ["Contents", "About", "At a Glance", "Specialization Options", "Tables", "Options"].indexOf(page_title) < 0){
+            const page_html = page.innerHTML;
+            const page_guid = page_title.toLowerCase().replaceAll(/[^a-z0-9]/g, "");
+            ex.books[book_guid].pages[page_guid] = {
+                "title": page_title,
+                "html": page_html
+            };
+        }
+    }
+
+    for (const opt of window.document.querySelectorAll("section.ability table")){
+        const name = opt.querySelector("thead").textContent.trim();
+        const effect = opt.querySelector("tbody").textContent.trim();
+        const guid = name.toLowerCase().replaceAll(/[^a-z0-9]/g, "");
+        ex[guid] = {
+            "name": name,
+            "effect": effect
+        };
+    }
+}
+
+fs.writeFileSync("public/export.json", JSON.stringify(ex));
